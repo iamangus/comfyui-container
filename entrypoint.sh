14e0528a773b3b1ee72ca6185d97503f45200b5d
iamangus <turnbull.angus@gmail.com>
2024-12-26 13:21:15 -0600

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,10 @@ if [ ! -d "/data/comfyui/models" ]; then
     python3 -m venv .venv
     source .venv/bin/activate
     pip install packaging==23.1 pygit2==1.12.2
+    pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126
+    pip install -r requirements.txt
 fi
 cd /data/comfyui
 source .venv/bin/activate
 echo "Launching..."
-python entry_with_update.py --listen 0.0.0.0 --port 8080 --theme dark
-
-
-pip install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu126
-
+python main.py --listen 0.0.0.0 --port 8188
